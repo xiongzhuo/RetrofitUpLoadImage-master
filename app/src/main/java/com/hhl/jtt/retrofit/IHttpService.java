e3dd61e287b8c9c8d3e822893a6e8a2777c1df4c
xiongzhuo <15388953585@163.com>
2017-07-05 16:59:31 +0800

    1.retrofit网络框架拍照和相册选取图片上传。

---
+++ app/src/main/java/com/hhl/jtt/retrofit/IHttpService.java
@@ -0,0 +1,29 @@
+package com.hhl.jtt.retrofit;
+
+import com.hhl.jtt.bean.BaseBean;
+
+import java.util.Map;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PartMap;
+
+/**
+ * Created by DELL on 2017/3/20.
+ * retrofit接口对象，
+ */
+
+public interface IHttpService {
+
+    /**
+     * 图片提交
+     *
+     * @param params
+     * @return
+     */
+    @Multipart
+    @POST("/finalapi/user/user/getImageUploads")
+    Call<BaseBean> upLoadAgree(@PartMap Map<String, RequestBody> params);
+}
---
+++ app/src/main/java/com/hhl/jtt/retrofit/UrlConfig.java
@@ -0,0 +1,12 @@
+package com.hhl.jtt.retrofit;
+
+
+/**
+ * Url 配置类,包含所有的接口访问地址
+ */
+public class UrlConfig {
+
+    //PC端接口
+    public static final String ROOT_URL = "http://smart.hnliuxing.com:8086/";
+
+}
